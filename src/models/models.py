a95ce197ae5e7ef7bee5ca444dc43f2c578b0c87
Rom1987 <maks3564195@gmail.com>
2026-09-11 19:23:56 +0300

    Добавление новых полей для моделей

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, date
 import enum
 from typing import Annotated
 
@@ -10,12 +10,12 @@ from src.db.db import Base
 metadata_obj = MetaData()
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime.datetime, mapped_column(
+created_at = Annotated[datetime, mapped_column(
     server_defaults=text("TIMEZONE('utc', now())"),
 )]
-updated_at = Annotated[datetime.datetime, mapped_column(
+updated_at = Annotated[datetime, mapped_column(
     server_defaults=text("TIMEZONE('utc', now())"),
-    onupdate=datetime.datetime.utcnow,
+    onupdate=datetime.utcnow,
 )]
 
 
@@ -23,19 +23,15 @@ class BudgetOrm(Base):
     __tablename__ = "budget"
 
     id: Mapped[intpk]
+    limit_amount: Mapped[float]
+    period_month: Mapped[date]
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
 
     category_id: Mapped[int | None] = mapped_column(ForeignKey("category.id", ondelete="SET NULL"))
     category: Mapped[list["CategoryOrm"]] = relationship(
-        # конкретно указали ссылку на таблицу
         back_populates="budget"
     )
-
-
-class CategoryType(enum.Enum):
-    income = "income"
-    expense = "expense"
 
 
 class CategoryOrm(Base):
@@ -43,20 +39,34 @@ class CategoryOrm(Base):
 
     id: Mapped[intpk]
     name: Mapped[str]
-    type: Mapped[CategoryType]
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
 
     budget: Mapped[list["BudgetOrm"]] = relationship(
         back_populates="category"
     )
+    transaction: Mapped[list["TransactionOrm"]] = relationship(
+        back_populates="category"
+    )
 
 
-class Transaction(Base):
+class TransactionType(enum.Enum):
+    income = "income"
+    expense = "expense"
+
+
+class TransactionOrm(Base):
     __tablename__ = "transaction"
 
     id: Mapped[intpk]
+    type: Mapped[TransactionType]
     sum: Mapped[float]
     description: Mapped[str | None]
+    occurred_on: Mapped[datetime]
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
+
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id", ondelete="CASCADE"))
+    category: Mapped[list["CategoryOrm"]] = relationship(
+        back_populates="transaction"
+    )
